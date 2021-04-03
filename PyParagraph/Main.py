33a7ed382300fabd65ff164b1ffb41bb754f5d6b
ANDRES ESTRADA <andrebestrada@gmail.com>
2021-04-03 11:10:25 -0600

    Modify PyParagraph

--- PyParagraph/Main.py
+++ PyParagraph/Main.py
@@ -12,7 +12,8 @@ number_of_lines = 0
 number_of_words = 0
 number_of_sentence = 0
 
-
+word_dic={}
+word_len=[]
 #    for cnt, line in enumerate(fp):
 #        print("Line {}: {}".format(cnt, line))
 with open(Text) as Paragraph:
@@ -20,21 +21,31 @@ with open(Text) as Paragraph:
         line = line.strip("\n")
 
         words = line.split()
+        for word in words:
+            word_len.append(len(word))
+
+
         print(words)
+        print(word_len)
         number_of_words += len(words)
-        print("----------------------------------------")
-        
+
+        print("\n")
+        print("\n")
         sentence=line.split('.')
         sentence=list(filter(None, sentence))
         # print(sentence)
         number_of_sentence += len(sentence)
         
 
+        letter_tot=sum(word_len)
+        letter_avg=letter_tot/number_of_words
+
+
 print("Paragraph Analysis")
 print("-----------------")
 print(f'Approximate Word Count: {number_of_words}')
 print(f'Approximate Sentence Count: {number_of_sentence}')
-# print(f'Average Letter Count: {}')
+print(f'Average Letter Count: {letter_avg}')
 # print(f'Average Sentence Length:{}')
 
 # Paragraph Analysis
