0e4ff233ec27235a210230ca0d0e8c7ad576464d
ANDRES ESTRADA <andrebestrada@gmail.com>
2021-04-04 22:27:28 -0500

    Complete HW

--- PyParagraph/Main.py
+++ PyParagraph/Main.py
@@ -8,12 +8,15 @@ import csv
 import re
 
 Text=os.path.join("Resources","Data_paragraph_1.txt")
+Text=os.path.join("Resources","Data_paragraph_2.txt")
 number_of_lines = 0
 number_of_words = 0
 number_of_sentence = 0
+words_sum=0
 
 word_dic={}
 word_len=[]
+sentence_len=[]
 #    for cnt, line in enumerate(fp):
 #        print("Line {}: {}".format(cnt, line))
 with open(Text) as Paragraph:
@@ -25,14 +28,24 @@ with open(Text) as Paragraph:
             word_len.append(len(word))
 
 
-        print(words)
-        print(word_len)
+        # print(words)
+        # print(word_len)
         number_of_words += len(words)
 
-        print("\n")
-        print("\n")
+        # print("\n")
+        # print("\n")
         sentence=line.split('.')
         sentence=list(filter(None, sentence))
+        # print(sentence)
+
+        for words in sentence:
+            oracion=words.split()
+            sentence_len.append(len(oracion))
+            words_sum+=len(oracion)
+
+        # print(".................................")
+
+
         # print(sentence)
         number_of_sentence += len(sentence)
         
@@ -41,12 +54,15 @@ with open(Text) as Paragraph:
         letter_avg=letter_tot/number_of_words
 
 
+sentence_len=words_sum/len(sentence)
+letter_avg=str(round(letter_avg,2))
+
 print("Paragraph Analysis")
-print("-----------------")
+print("-------------------")
 print(f'Approximate Word Count: {number_of_words}')
 print(f'Approximate Sentence Count: {number_of_sentence}')
 print(f'Average Letter Count: {letter_avg}')
-# print(f'Average Sentence Length:{}')
+print(f'Average Sentence Length: {sentence_len}')
 
 # Paragraph Analysis
 # -----------------
